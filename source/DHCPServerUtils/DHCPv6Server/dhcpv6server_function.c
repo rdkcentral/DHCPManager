7a377e938946af79396b26828ef8e36eb0f262c0
pdas004c <Pradeepta_Das@cable.comcast.com>
2023-04-24 20:50:24 +0000

    RDKCOM-3844: RDKBDEV-1731: control dibbler log level based on a syscfg variable
    Reason for change:
    service_ipv6: control dibbler log level based on a syscfg variable
    
    Size of dibbler-client.log increases with every wan-restart
    
    BUG DESCRIPTION:
    size of dibbler log file increases for wan-restart since there is no scenario for handling the log rotation.
    
    FIX DESCRIPTION:
    Introduce dibbler_log_level syscfg variable to set dibbler log-level
    in configuration file and set default log level as 4 (Error) in
    syscfg db. This syscfg variable can be changed to other log-level for
    debugging purpose.
    
    From the dibbler documentation ( https://klub.com.pl/dhcpv6/doc/dibbler-user.pdf ):
    
      log-level:
    
      Takes one integer parameter. Defines verbose level of the log
      messages. The valid range is from 1 (very quiet) to 8 (very
      verbose). Those values are modelled after levels used in syslog.
      These are:
    
        1 : Emergency
        2 : Alert
        3 : Critical
        4 : Error
        5 : Warning
        6 : Notice
        7 : Info
        8 : Debug
    
      Currently Dibbler is using levels 3 to 8, as 1 and 2 are reserved
      for system wide emergency event
    
    Test Procedure:
    changed dibbler_log_level syscfg varibale and tested it for different log-level.
    
    Risks: None.
    
    Signed-off-by: Pradeepta Das <Pradeepta_Das@comcast.com>
    Change-Id: I61460ae7bc8a638433b9442d1a1d067602eb1154

--- source/DHCPServerUtils/DHCPv6Server/dhcpv6server_function.c
+++ source/DHCPServerUtils/DHCPv6Server/dhcpv6server_function.c
@@ -599,9 +599,35 @@ void __cosa_dhcpsv6_refresh_config()
     errno_t rc = -1;
     if (!fp)
         goto EXIT;
-    /*Begin write configuration */
-    fprintf(fp, "log-level 4\n");
 
+    /*Begin write configuration */
+    {
+        char buf[12];
+        int log_level = 4;
+        if (syscfg_get( NULL, "dibbler_log_level", buf, sizeof(buf)) == 0)
+        {
+            log_level = atoi(buf);
+        }
+        if (log_level < 1)
+        {
+            log_level = 1;
+        }
+        else if (log_level > 8)
+        {
+            log_level = 4;
+        }
+        /*
+            1 : Emergency (not used - logging will be disabled)
+            2 : Alert (not used - logging will be disabled)
+            3 : Critical
+            4 : Error
+            5 : Warning
+            6 : Notice
+            7 : Info
+            8 : Debug
+        */
+        fprintf(fp, "log-level %d\n", log_level);
+    }
     /*
        Enable inactive mode: When server begins operation and it detects that
        required interfaces are not ready, error message is printed and server
