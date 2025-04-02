33f219d4ae7ec8aa05f1a6ba7fb302b797349e90
S-Parthiban-Selvaraj <parthiban.selvaraj@sky.uk>
2025-04-02 10:59:18 +0000

    Adding sysevent set
    Signed-off-by: S-Parthiban-Selvaraj <parthiban.selvaraj@sky.uk>

--- source/DHCPMgrUtils/dhcpmgr_custom_options.c
+++ source/DHCPMgrUtils/dhcpmgr_custom_options.c
@@ -20,6 +20,7 @@
 
 #include "dhcpmgr_custom_options.h"
 #include "util.h"
+#include "ifl.h"
 
 static int DhcpMgr_Option17Set_Common(const char *ifName, const char *OptionValue);
 
@@ -87,6 +88,97 @@ __attribute__((weak)) int Set_DhcpV4_CustomOption43(const char *ifName, const ch
 }
 
 
+/* The following functions are implemented based on the reference from the dibbler notify script file */
+/**
+ * @brief Parses the DHCP option 17 suboption based on the provided DHCP option value and IP mode.
+ *
+ * @param dhcp_option_val A pointer to a string containing the DHCP option value to be parsed.
+ * @param ip_mode A pointer to a string specifying the IP mode (e.g., IPv4 or IPv6).
+ * 
+ * @return A pointer to a dynamically allocated string containing the parsed suboption value.
+ *         The caller is responsible for freeing the allocated memory.
+ */
+static char* parse_dhcp_17_suboption(const char *dhcp_option_val, const char *ip_mode) 
+{
+    if (dhcp_option_val == NULL || ip_mode == NULL) 
+    {
+        DHCPMGR_LOG_ERROR("%s %d: Invalid arguments..\n", __FUNCTION__, __LINE__);
+        return NULL;
+    }
+
+    char *option_format = NULL;
+    char *dhcp_option_copy = strdup(dhcp_option_val);
+    if (dhcp_option_copy == NULL) 
+    {
+        DHCPMGR_LOG_ERROR("%s %d: Memory allocation failed..\n", __FUNCTION__, __LINE__);
+        return NULL;
+    }
+
+    // Remove colons from the input string
+    for (char *p = dhcp_option_copy; *p; ++p) 
+    {
+        if (*p == ':') 
+        {
+            *p = ' ';
+        }
+    }
+
+    int val = 2;
+    int len = (strcmp(ip_mode, "v4") == 0) ? 2 : 4;
+
+    char *remaining = dhcp_option_copy;
+    while (*remaining != '\0') 
+    {
+        char suboption[16] = {0};
+        strncpy(suboption, remaining, len);
+        remaining += len;
+
+        char suboption_length_str[16] = {0};
+        strncpy(suboption_length_str, remaining, len);
+        remaining += len;
+
+        int suboption_length = (int)strtol(suboption_length_str, NULL, 16);
+        int length = suboption_length * val;
+
+        char suboption_value[256] = {0};
+        strncpy(suboption_value, remaining, length);
+        remaining += length;
+
+        char *new_format = NULL;
+        if(option_format== NULL)
+        {
+            size_t new_format_len = strlen(suboption) + strlen(suboption_value) + 2; // '=' and '\0'
+            new_format = malloc(new_format_len);
+            if (new_format != NULL) 
+            {
+                snprintf(new_format, new_format_len, "%s=%s", suboption, suboption_value);
+            }
+        } 
+        else 
+        {
+            size_t new_format_len = strlen(option_format) + strlen(suboption) + strlen(suboption_value) + 3; // ' ', '=' and '\0'
+            new_format = malloc(new_format_len);
+            if (new_format != NULL) 
+            {
+                snprintf(new_format, new_format_len, "%s %s=%s", option_format, suboption, suboption_value);
+            }
+            free(option_format);
+        }
+
+        if (new_format == NULL) 
+        {
+            DHCPMGR_LOG_ERROR("%s %d: Memory allocation failed..\n", __FUNCTION__, __LINE__);
+            free(dhcp_option_copy);
+            return NULL;
+        }
+
+        option_format = new_format;
+    }
+
+    free(dhcp_option_copy);
+    return option_format;
+}
+
 /**
  * @brief Processes and sets DHCP Option 17 suboptions for a given interface.
  *
@@ -143,21 +235,22 @@ static int DhcpMgr_Option17Set_Common(const char *ifName, const char *OptionValu
             if (strcmp(suboption, "vendor") == 0) 
             {
                 DHCPMGR_LOG_INFO("Suboption vendor-id is %s in option %s\n", suboption_data, OptionValue);
-                sysevent_set("ipv6-vendor-id", suboption_data);
+                ifl_set_event("ipv6-vendor-id", suboption_data);
             } else if (strcmp(suboption, "38") == 0) 
             {
                 DHCPMGR_LOG_INFO("Suboption TimeOffset is %s in option %s\n", suboption_data, OptionValue);
-                sysevent_set("ipv6-timeoffset", suboption_data);
+                ifl_set_event("ipv6-timeoffset", suboption_data);
                 // Additional processing for TimeOffset can be added here
             } else if (strcmp(suboption, "39") == 0) 
             {
                 DHCPMGR_LOG_INFO("Suboption IP Mode Preference is %s in option %s\n", suboption_data, OptionValue);
-                sysevent_set("wan6_ippref", suboption_data);
-                char *mta_ip_pref = sysevent_get("MTA_IP_PREF");
-                if (mta_ip_pref == NULL || strlen(mta_ip_pref) == 0) 
+                ifl_set_event("wan6_ippref", suboption_data);
+                char mta_ip_pref[64] = {0};
+                ifl_get_event("MTA_IP_PREF", mta_ip_pref, sizeof(mta_ip_pref));
+                if (strlen(mta_ip_pref) == 0) 
                 {
                     DHCPMGR_LOG_INFO("Setting MTA_IP_PREF value to %s\n", suboption_data);
-                    sysevent_set("MTA_IP_PREF", suboption_data);
+                    ifl_set_event("MTA_IP_PREF", suboption_data);
                     mta_dhcp_option_received = 1;
                 } else 
                 {
@@ -166,14 +259,14 @@ static int DhcpMgr_Option17Set_Common(const char *ifName, const char *OptionValu
             } else if (strcmp(suboption, "2") == 0) 
             {
                 DHCPMGR_LOG_INFO("Suboption Device Type is %s in option %s\n", suboption_data, OptionValue);
-                sysevent_set("ipv6-device-type", suboption_data);
+                ifl_set_event("ipv6-device-type", suboption_data);
             } else if (strcmp(suboption, "3") == 0) 
             {
                 DHCPMGR_LOG_INFO("Suboption List of Embedded Components in eDOCSIS Device is %s in option %s\n", suboption_data, OptionValue);
-                sysevent_set("ipv6-embd-comp-in-device", suboption_data);
+                ifl_set_event("ipv6-embd-comp-in-device", suboption_data);
             } else if (strcmp(suboption, "2170") == 0 || strcmp(suboption, "2171") == 0) 
             {
-                char *parsed_value = parse_dhcp_option(suboption_data, "v6");
+                char *parsed_value = parse_dhcp_17_suboption(suboption_data, "v6"); //parsed_value should be freed
                 if (parsed_value) 
                 {
                     char *val_token = strtok(parsed_value, " ");
@@ -184,24 +277,24 @@ static int DhcpMgr_Option17Set_Common(const char *ifName, const char *OptionValu
                         if (subopt && subopt_data) {
                             if (strcmp(subopt, "0001") == 0) 
                             {
-                                char *primary_address = (strcmp(suboption, "2170") == 0) ? 
-                                    sysevent_get("MTA_DHCPv4_PrimaryAddress") : sysevent_get("MTA_DHCPv6_PrimaryAddress");
+                                char primary_address[64] = {0};
+                                ifl_get_event((strcmp(suboption, "2170") == 0) ? "MTA_DHCPv4_PrimaryAddress" : "MTA_DHCPv6_PrimaryAddress", primary_address, sizeof(primary_address));
                                 
-                                    if (primary_address == NULL || strlen(primary_address) == 0) 
+                                if (strlen(primary_address) == 0) 
                                 {
                                     DHCPMGR_LOG_INFO("Setting PrimaryAddress value as %s\n", subopt_data);
-                                    sysevent_set((strcmp(suboption, "2170") == 0) ? "MTA_DHCPv4_PrimaryAddress" : "MTA_DHCPv6_PrimaryAddress", subopt_data);
+                                    ifl_set_event((strcmp(suboption, "2170") == 0) ? "MTA_DHCPv4_PrimaryAddress" : "MTA_DHCPv6_PrimaryAddress", subopt_data);
                                     mta_dhcp_option_received = 1;
                                 }
                             } else if (strcmp(subopt, "0002") == 0) 
                             {
-                                char *secondary_address = (strcmp(suboption, "2170") == 0) ? 
-                                    sysevent_get("MTA_DHCPv4_SecondaryAddress") : sysevent_get("MTA_DHCPv6_SecondaryAddress");
+                                char secondary_address[64] = {0};
+                                ifl_get_event((strcmp(suboption, "2170") == 0) ? "MTA_DHCPv4_SecondaryAddress" : "MTA_DHCPv6_SecondaryAddress", secondary_address, sizeof(secondary_address));
                                 
-                                    if (secondary_address == NULL || strlen(secondary_address) == 0) 
+                                if (strlen(secondary_address) == 0) 
                                 {
                                     DHCPMGR_LOG_INFO("Setting SecondaryAddress value as %s\n", subopt_data);
-                                    sysevent_set((strcmp(suboption, "2170") == 0) ? "MTA_DHCPv4_SecondaryAddress" : "MTA_DHCPv6_SecondaryAddress", subopt_data);
+                                    ifl_set_event((strcmp(suboption, "2170") == 0) ? "MTA_DHCPv4_SecondaryAddress" : "MTA_DHCPv6_SecondaryAddress", subopt_data);
                                     mta_dhcp_option_received = 1;
                                 }
                             }
@@ -218,7 +311,7 @@ static int DhcpMgr_Option17Set_Common(const char *ifName, const char *OptionValu
     if (mta_dhcp_option_received) 
     {
         DHCPMGR_LOG_INFO("Setting dhcp_mta_option event as received\n");
-        sysevent_set("dhcp_mta_option", "received");
+        ifl_set_event("dhcp_mta_option", "received");
     }
 
     free(srv_option17);
